fcaaf508370b6d68c35d1bd6a1f046f3700a30d9
fuaberu <86504230+fuaberu@users.noreply.github.com>
2024-01-17 08:04:52 -0300

    Fix h2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="mx-auto mt-24 max-w-fit">
 	<span class="max-w-fit text-teal-300">👋 Hi, I am</span>
 	<h1 class="max-w-fit text-4xl sm:text-7xl">Kevin Alves Fabel</h1>
-	<h2 class="max-w-fit text-3xl sm:text-6xl text-gray-400 font-extrabold">software engineer</h2>
+	<h2 class="max-w-fit text-3xl sm:text-6xl text-gray-400 font-extrabold">Software Engineer</h2>
 	<a
 		class="mt-6 inline-block rounded-md bg-teal-300 px-4 py-2 font-bold text-gray-800"
 		href="/about">Meet Kevin!</a
